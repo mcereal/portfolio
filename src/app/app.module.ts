ab85722b7f11ed74ea479ce3f4dde08543f5e451
mcereal <mpbcurtis@gmail.com>
2020-10-26 15:15:56 -0400

    modals created for porfolio section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NgxPageScrollCoreModule } from 'ngx-page-scroll-core';
+import { PortfolioModalComponent } from './portfolio-modal/portfolio-modal.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { NgxPageScrollCoreModule } from 'ngx-page-scroll-core';
     HeaderComponent,
     AboutComponent,
     FooterComponent,
+    PortfolioModalComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.css']
+  styleUrls: ['./portfolio.component.css'],
 })
 export class PortfolioComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
